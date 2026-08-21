562756fd57f991a2b77c8c4c68533cad6376e63a
Akhil Dixit <dixitakhil210@gmail.com>
2026-08-21 19:59:47 +0530

    Create 2824. Count Pairs Whose Sum is Less than Target.cpp

---
+++ 2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n=nums.size();
+        int count =0;
+        for(int i=0 ;i<n-1;i++){
+
+           for(int j=i+1;j<n;j++){
+            int sum = nums[i]+nums[j];
+            
+            if(target>sum){
+                count++ ;
+            }
+           }
+        }
+        return count;
+        
+    }
+};
