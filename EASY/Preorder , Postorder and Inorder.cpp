94e0e96a71e8659f7bc145565857b65b4a4b4a6b
Akhil Dixit <dixitakhil210@gmail.com>
2026-09-03 22:02:01 +0530

    Create Preorder , Postorder and Inorder.cpp

---
+++ EASY/Preorder , Postorder and Inorder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+// Inorder: Left -> Root -> Right
+void inorder(Node* root) {
+    if (root == nullptr) return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// Preorder: Root -> Left -> Right
+void preorder(Node* root) {
+    if (root == nullptr) return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Postorder: Left -> Right -> Root
+void postorder(Node* root) {
+    if (root == nullptr) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
